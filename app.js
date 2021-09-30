2105c0063f808d758f1889164ab9e91a229754ad
mahmoud swedani <mahmoudkasimswedani@gmail.com>
2021-09-30 05:38:19 +0300

    fix script to run project

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-import * as React from "https://cdn.skypack.dev/react@17.0.1";
-import * as ReactDOM from "https://cdn.skypack.dev/react-dom@17.0.1";
-
 const firstSoundsGroup = [
     {
       keyCode: 81,
